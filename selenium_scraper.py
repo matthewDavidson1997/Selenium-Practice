22144aebf4f0d53f5651af8bdf4caef750c16d2a
James Davidson <j.davidson@vernalis.com>
2022-10-15 11:59:58 +0100

    Removed overridden selium.webdriver import

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
